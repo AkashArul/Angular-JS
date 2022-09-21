f36c61ecd5c9de4b8071b5057fa0928afcaf1db4
Akash Krishnan <39428376+AkashArul@users.noreply.github.com>
2022-09-21 05:22:42 -0700

    State and city

---
+++ State and city/city.js
@@ -0,0 +1,12 @@
+function state()
+{
+    let a=document.getElementById("state").value;
+    document.getElementById("city").value=a;
+    console.log(a);
+}
+function city()
+{
+    let b=document.getElementById("city").value;
+    document.getElementById("state").value=b;
+    console.log(b);
+}
